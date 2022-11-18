1be8d1584a1ef9543339ba05755211da71dcd7a9
Ken Nguyen <khanh.quoc.nguyen@mgm-tp.com>
2022-11-18 15:20:05 +0700

    add helm, kubernetes provider

--- eks.tf
+++ eks.tf
@@ -90,9 +90,6 @@ module "cluster" {
   tags = merge({
       "Name" = local.cluster_name
   }, var.tags)
-
-  write_kubeconfig   = true
-  config_output_path = "./"
 }
 
 # set spot fleet Autoscaling policy
--- provider.tf
+++ provider.tf
@@ -14,11 +14,11 @@ provider "aws" {
 }
 
 # Get EKS authentication for being able to manage k8s objects from terraform
-# provider "kubernetes" {
-#   host                   = module.cluster.cluster_endpoint
-#   cluster_ca_certificate = base64decode(module.cluster.certificate_authority_data)
-#   token                  = data.aws_eks_cluster_auth.cluster.token
-# }
+provider "kubernetes" {
+  host                   = module.cluster.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.cluster.certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
 
 # provider "helm" {
 #   kubernetes {
