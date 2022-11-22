fef4346e5d782558cfa4ea5f8bf022b8c04ad4ab
Ken Nguyen <khanh.quoc.nguyen@mgm-tp.com>
2022-11-22 16:47:25 +0700

    add aws_eks_cluster, aws_eks_cluster_auth data

--- provider.tf
+++ provider.tf
@@ -13,40 +13,40 @@ provider "aws" {
   region = var.region
 }
 
-# # Get EKS cluster info to configure Kubernetes and Helm providers
-# data "aws_eks_cluster" "cluster" {
-#   name = local.cluster_name
-# }
+# Get EKS cluster info to configure Kubernetes and Helm providers
+data "aws_eks_cluster" "cluster" {
+  name = local.cluster_name
+}
 data "aws_eks_cluster_auth" "cluster" {
   name = local.cluster_name
 }
 
-# # Get EKS authentication for being able to manage k8s objects from terraform
-# provider "kubernetes" {
-#   host                   = data.aws_eks_cluster.cluster.endpoint
-#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-#   token                  = data.aws_eks_cluster_auth.cluster.token
-# }
-
-# provider "helm" {
-#   kubernetes {
-#     host                   = data.aws_eks_cluster.cluster.endpoint
-#     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-#     token                  = data.aws_eks_cluster_auth.cluster.token
-#   }
-# }
-
+# Get EKS authentication for being able to manage k8s objects from terraform
 provider "kubernetes" {
-  host                   = module.cluster.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.cluster.cluster_certificate_authority_data)
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
 provider "helm" {
   kubernetes {
-    host                   = module.cluster.cluster_endpoint
-    cluster_ca_certificate = base64decode(module.cluster.cluster_certificate_authority_data)
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
     token                  = data.aws_eks_cluster_auth.cluster.token
   }
 }
+
+# provider "kubernetes" {
+#   host                   = module.cluster.cluster_endpoint
+#   cluster_ca_certificate = base64decode(module.cluster.cluster_certificate_authority_data)
+#   token                  = data.aws_eks_cluster_auth.cluster.token
+# }
+
+# provider "helm" {
+#   kubernetes {
+#     host                   = module.cluster.cluster_endpoint
+#     cluster_ca_certificate = base64decode(module.cluster.cluster_certificate_authority_data)
+#     token                  = data.aws_eks_cluster_auth.cluster.token
+#   }
+# }
 
