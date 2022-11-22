0c5682cd8071025f59bcf6e9c332c885e5dc681b
Ken Nguyen <khanh.quoc.nguyen@mgm-tp.com>
2022-11-22 14:57:53 +0700

    change eks managed node groups instance types

--- argocd_helm_release.tf
+++ argocd_helm_release.tf
@@ -6,6 +6,7 @@ resource "helm_release" "argocd" {
   version           = "5.14.1"
   namespace         = "argocd"
   create_namespace  = true
+  cleanup_on_fail   = true
 
   values = [
     file("argocd/application.yml")
