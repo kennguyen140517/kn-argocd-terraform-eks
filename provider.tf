068343190c0d9d624aba6adde4981b55658e05f7
Ken Nguyen <khanh.quoc.nguyen@mgm-tp.com>
2022-11-22 13:49:01 +0700

    edit helm and k8s provider

--- provider.tf
+++ provider.tf
@@ -13,25 +13,38 @@ provider "aws" {
   region = var.region
 }
 
-# Get EKS cluster info to configure Kubernetes and Helm providers
-data "aws_eks_cluster" "cluster" {
-  name = local.cluster_name
-}
-data "aws_eks_cluster_auth" "cluster" {
-  name = local.cluster_name
-}
+# # Get EKS cluster info to configure Kubernetes and Helm providers
+# data "aws_eks_cluster" "cluster" {
+#   name = local.cluster_name
+# }
+# data "aws_eks_cluster_auth" "cluster" {
+#   name = local.cluster_name
+# }
 
-# Get EKS authentication for being able to manage k8s objects from terraform
+# # Get EKS authentication for being able to manage k8s objects from terraform
+# provider "kubernetes" {
+#   host                   = data.aws_eks_cluster.cluster.endpoint
+#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+#   token                  = data.aws_eks_cluster_auth.cluster.token
+# }
+
+# provider "helm" {
+#   kubernetes {
+#     host                   = data.aws_eks_cluster.cluster.endpoint
+#     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+#     token                  = data.aws_eks_cluster_auth.cluster.token
+#   }
+# }
+
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
+  host                   = module.cluster.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 }
 
 provider "helm" {
   kubernetes {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-    token                  = data.aws_eks_cluster_auth.cluster.token
+    host                   = module.cluster.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   }
 }
+
