0336ff711ee897abdc36c1a0d184cf0b1c8f4000
Ken Nguyen <khanh.quoc.nguyen@mgm-tp.com>
2022-11-28 16:14:48 +0700

    remove provider helm and kubernetes

--- provider.tf
+++ provider.tf
@@ -12,34 +12,3 @@ provider "aws" {
   alias  = "withoutProviderTags"
   region = var.region
 }
-
-
-# # Get EKS authentication for being able to manage k8s objects from terraform
-# provider "kubernetes" {
-#   host                   = data.aws_eks_cluster.cluster.endpoint
-#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-#   token                  = data.aws_eks_cluster_auth.cluster.token
-# }
-
-# provider "helm" {
-#   kubernetes {
-#     host                   = data.aws_eks_cluster.cluster.endpoint
-#     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-#     token                  = data.aws_eks_cluster_auth.cluster.token
-#   }
-# }
-
-# provider "kubernetes" {
-#   host                   = module.cluster.cluster_endpoint
-#   cluster_ca_certificate = base64decode(module.cluster.cluster_certificate_authority_data)
-#   token                  = data.aws_eks_cluster_auth.cluster.token
-# }
-
-# provider "helm" {
-#   kubernetes {
-#     host                   = module.cluster.cluster_endpoint
-#     cluster_ca_certificate = base64decode(module.cluster.cluster_certificate_authority_data)
-#     token                  = data.aws_eks_cluster_auth.cluster.token
-#   }
-# }
-
