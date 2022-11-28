d823fde5f7b622e7be5e6120a0330dbdab23e864
Ken Nguyen <khanh.quoc.nguyen@mgm-tp.com>
2022-11-28 16:35:46 +0700

    remove alls

--- main.tf
+++ main.tf
@@ -1,91 +1,91 @@
-module "main_vpc" {
-  source = "./modules/00-vpc"
+# module "main_vpc" {
+#   source = "./modules/00-vpc"
 
-  region                  = var.region
-  master_prefix           = var.master_prefix
-  env_prefix              = var.env_prefix
-  app_prefix              = var.app_prefix
-  allowed_account_ids     = var.allowed_account_ids
-  vpc_cidr                = var.vpc_cidr
-  subnet_prefix_extension = var.subnet_prefix_extension
-  zone_offset             = var.zone_offset
+#   region                  = var.region
+#   master_prefix           = var.master_prefix
+#   env_prefix              = var.env_prefix
+#   app_prefix              = var.app_prefix
+#   allowed_account_ids     = var.allowed_account_ids
+#   vpc_cidr                = var.vpc_cidr
+#   subnet_prefix_extension = var.subnet_prefix_extension
+#   zone_offset             = var.zone_offset
 
-  tags = var.tags
-}
+#   tags = var.tags
+# }
 
-module "main_eks" {
-  source = "./modules/01-eks"
+# module "main_eks" {
+#   source = "./modules/01-eks"
 
-  region              = var.region
-  master_prefix       = var.master_prefix
-  env_prefix          = var.env_prefix
-  app_prefix          = var.app_prefix
-  allowed_account_ids = var.allowed_account_ids
+#   region              = var.region
+#   master_prefix       = var.master_prefix
+#   env_prefix          = var.env_prefix
+#   app_prefix          = var.app_prefix
+#   allowed_account_ids = var.allowed_account_ids
 
-  vpc_id                  = module.main_vpc.vpc_id
-  subnet_ids              = module.main_vpc.private_subnets
-  create_eks              = var.create_eks
-  cluster_version         = var.cluster_version
-  enable_irsa             = var.enable_irsa
-  eks_managed_node_groups = var.eks_managed_node_groups
-  autoscaling_average_cpu = var.autoscaling_average_cpu
+#   vpc_id                  = module.main_vpc.vpc_id
+#   subnet_ids              = module.main_vpc.private_subnets
+#   create_eks              = var.create_eks
+#   cluster_version         = var.cluster_version
+#   enable_irsa             = var.enable_irsa
+#   eks_managed_node_groups = var.eks_managed_node_groups
+#   autoscaling_average_cpu = var.autoscaling_average_cpu
 
-  tags = var.tags
-}
+#   tags = var.tags
+# }
 
-module "aws_load_balancer_controller" {
-  source = "./modules/02-aws-load-balancer-controller"
+# module "aws_load_balancer_controller" {
+#   source = "./modules/02-aws-load-balancer-controller"
 
-  region              = var.region
-  master_prefix       = var.master_prefix
-  env_prefix          = var.env_prefix
-  app_prefix          = var.app_prefix
-  allowed_account_ids = var.allowed_account_ids
+#   region              = var.region
+#   master_prefix       = var.master_prefix
+#   env_prefix          = var.env_prefix
+#   app_prefix          = var.app_prefix
+#   allowed_account_ids = var.allowed_account_ids
 
-  cluster_name            = module.main_eks.cluster_name
-  ingress_controller_name = "aws-load-balancer-controller"
+#   cluster_name            = module.main_eks.cluster_name
+#   ingress_controller_name = "aws-load-balancer-controller"
 
-  tags = var.tags
-}
+#   tags = var.tags
+# }
 
-module "ingress_nginx_controller" {
-  source = "./modules/03-ingress-nginx-controller"
+# module "ingress_nginx_controller" {
+#   source = "./modules/03-ingress-nginx-controller"
 
-  region              = var.region
-  master_prefix       = var.master_prefix
-  env_prefix          = var.env_prefix
-  app_prefix          = var.app_prefix
-  allowed_account_ids = var.allowed_account_ids
+#   region              = var.region
+#   master_prefix       = var.master_prefix
+#   env_prefix          = var.env_prefix
+#   app_prefix          = var.app_prefix
+#   allowed_account_ids = var.allowed_account_ids
 
-  cluster_name = module.main_eks.cluster_name
+#   cluster_name = module.main_eks.cluster_name
 
-  tags = var.tags
-}
+#   tags = var.tags
+# }
 
-module "cert_manager" {
-  source = "./modules/04-cert-manager"
+# module "cert_manager" {
+#   source = "./modules/04-cert-manager"
 
-  region              = var.region
-  master_prefix       = var.master_prefix
-  env_prefix          = var.env_prefix
-  app_prefix          = var.app_prefix
-  allowed_account_ids = var.allowed_account_ids
+#   region              = var.region
+#   master_prefix       = var.master_prefix
+#   env_prefix          = var.env_prefix
+#   app_prefix          = var.app_prefix
+#   allowed_account_ids = var.allowed_account_ids
 
-  cluster_name = module.main_eks.cluster_name
+#   cluster_name = module.main_eks.cluster_name
 
-  tags = var.tags
-}
+#   tags = var.tags
+# }
 
-module "argocd" {
-  source = "./modules/05-argocd"
+# module "argocd" {
+#   source = "./modules/05-argocd"
 
-  region              = var.region
-  master_prefix       = var.master_prefix
-  env_prefix          = var.env_prefix
-  app_prefix          = var.app_prefix
-  allowed_account_ids = var.allowed_account_ids
+#   region              = var.region
+#   master_prefix       = var.master_prefix
+#   env_prefix          = var.env_prefix
+#   app_prefix          = var.app_prefix
+#   allowed_account_ids = var.allowed_account_ids
 
-  cluster_name = module.main_eks.cluster_name
+#   cluster_name = module.main_eks.cluster_name
 
-  tags = var.tags
-}
+#   tags = var.tags
+# }
--- modules/02-aws-load-balancer-controller/aws_load_balancer_controller.tf
+++ modules/02-aws-load-balancer-controller/aws_load_balancer_controller.tf
@@ -1,44 +1,44 @@
-locals {
-  ingress_controller_iam_role = format("%s-eks-alb-controller-role", local.general_prefix)
-}
+# locals {
+#   ingress_controller_iam_role = format("%s-eks-alb-controller-role", local.general_prefix)
+# }
 
-resource "kubernetes_service_account" "aws_load_balancer_controller" {
-  metadata {
-    name      = var.ingress_controller_name
-    namespace = "kube-system"
+# resource "kubernetes_service_account" "aws_load_balancer_controller" {
+#   metadata {
+#     name      = var.ingress_controller_name
+#     namespace = "kube-system"
 
-    labels = {
-      "app.kubernetes.io/component" = "controller"
-      "app.kubernetes.io/name"      = var.ingress_controller_name
-    }
+#     labels = {
+#       "app.kubernetes.io/component" = "controller"
+#       "app.kubernetes.io/name"      = var.ingress_controller_name
+#     }
 
-    annotations = {
-      "eks.amazonaws.com/role-arn" = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${local.ingress_controller_iam_role}"
-    }
-  }
-}
+#     annotations = {
+#       "eks.amazonaws.com/role-arn" = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${local.ingress_controller_iam_role}"
+#     }
+#   }
+# }
 
-resource "helm_release" "aws_load_balancer_controller" {
-  name = format("%s-aws-load-balancer-controller", local.general_prefix)
+# resource "helm_release" "aws_load_balancer_controller" {
+#   name = format("%s-aws-load-balancer-controller", local.general_prefix)
 
-  repository      = "https://aws.github.io/eks-charts"
-  chart           = "aws-load-balancer-controller"
-  version         = "1.4.6"
-  namespace       = "kube-system"
-  cleanup_on_fail = true
+#   repository      = "https://aws.github.io/eks-charts"
+#   chart           = "aws-load-balancer-controller"
+#   version         = "1.4.6"
+#   namespace       = "kube-system"
+#   cleanup_on_fail = true
 
-  set {
-    name  = "clusterName"
-    value = var.cluster_name
-  }
+#   set {
+#     name  = "clusterName"
+#     value = var.cluster_name
+#   }
 
-  set {
-    name  = "serviceAccount.name"
-    value = kubernetes_service_account.aws_load_balancer_controller.metadata.0.name
-  }
+#   set {
+#     name  = "serviceAccount.name"
+#     value = kubernetes_service_account.aws_load_balancer_controller.metadata.0.name
+#   }
 
-  set {
-    name  = "serviceAccount.create"
-    value = "false"
-  }
-}
+#   set {
+#     name  = "serviceAccount.create"
+#     value = "false"
+#   }
+# }
