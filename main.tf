2dea16620fdc8a1392ff1f5fad7c9cdde5aacf18
Ken Nguyen <khanh.quoc.nguyen@mgm-tp.com>
2022-11-29 13:16:21 +0700

    deploy vpc and eks

--- main.tf
+++ main.tf
@@ -1,37 +1,37 @@
-# module "main_vpc" {
-#   source = "./modules/00-vpc"
+module "main_vpc" {
+  source = "./modules/00-vpc"
 
-#   region                  = var.region
-#   master_prefix           = var.master_prefix
-#   env_prefix              = var.env_prefix
-#   app_prefix              = var.app_prefix
-#   allowed_account_ids     = var.allowed_account_ids
-#   vpc_cidr                = var.vpc_cidr
-#   subnet_prefix_extension = var.subnet_prefix_extension
-#   zone_offset             = var.zone_offset
+  region                  = var.region
+  master_prefix           = var.master_prefix
+  env_prefix              = var.env_prefix
+  app_prefix              = var.app_prefix
+  allowed_account_ids     = var.allowed_account_ids
+  vpc_cidr                = var.vpc_cidr
+  subnet_prefix_extension = var.subnet_prefix_extension
+  zone_offset             = var.zone_offset
 
-#   tags = var.tags
-# }
+  tags = var.tags
+}
 
-# module "main_eks" {
-#   source = "./modules/01-eks"
+module "main_eks" {
+  source = "./modules/01-eks"
 
-#   region              = var.region
-#   master_prefix       = var.master_prefix
-#   env_prefix          = var.env_prefix
-#   app_prefix          = var.app_prefix
-#   allowed_account_ids = var.allowed_account_ids
+  region              = var.region
+  master_prefix       = var.master_prefix
+  env_prefix          = var.env_prefix
+  app_prefix          = var.app_prefix
+  allowed_account_ids = var.allowed_account_ids
 
-#   vpc_id                  = module.main_vpc.vpc_id
-#   subnet_ids              = module.main_vpc.private_subnets
-#   create_eks              = var.create_eks
-#   cluster_version         = var.cluster_version
-#   enable_irsa             = var.enable_irsa
-#   eks_managed_node_groups = var.eks_managed_node_groups
-#   autoscaling_average_cpu = var.autoscaling_average_cpu
+  vpc_id                  = module.main_vpc.vpc_id
+  subnet_ids              = module.main_vpc.private_subnets
+  create_eks              = var.create_eks
+  cluster_version         = var.cluster_version
+  enable_irsa             = var.enable_irsa
+  eks_managed_node_groups = var.eks_managed_node_groups
+  autoscaling_average_cpu = var.autoscaling_average_cpu
 
-#   tags = var.tags
-# }
+  tags = var.tags
+}
 
 # module "aws_load_balancer_controller" {
 #   source = "./modules/02-aws-load-balancer-controller"
