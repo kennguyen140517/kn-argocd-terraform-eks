a199c69bc641828b7471fa31ad9750e9a0817815
Ken Nguyen <khanh.quoc.nguyen@mgm-tp.com>
2022-11-29 14:43:24 +0700

    deploy aws load balancer controller, ingress nginx controller

--- locals.tf
+++ locals.tf
@@ -1,5 +1,9 @@
 locals {
-  adjusted_number_of_az = var.number_of_az <= length(data.aws_availability_zones.available.names) ? var.number_of_az : length(data.aws_availability_zones.available.names) 
-  lc_name_list = slice(data.aws_availability_zones.available.names, 0, local.adjusted_number_of_az)
-  general_prefix = format("%s-%s-%s", var.master_prefix, var.env_prefix, var.app_prefix)  
+  adjusted_number_of_az = var.number_of_az <= length(data.aws_availability_zones.available.names) ? var.number_of_az : length(data.aws_availability_zones.available.names)
+  lc_name_list          = slice(data.aws_availability_zones.available.names, 0, local.adjusted_number_of_az)
+  general_prefix        = format("%s-%s-%s", var.master_prefix, var.env_prefix, var.app_prefix)
+}
+
+locals {
+  cluster_name = format("%s-cluster", local.general_prefix)
 }
