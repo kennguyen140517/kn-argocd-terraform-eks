0a6c69944ddac208e16528a8079d0904498f488d
Ken Nguyen <khanh.quoc.nguyen@mgm-tp.com>
2022-11-29 14:42:06 +0700

    deploy aws load balancer controller, ingress nginx controller

--- data.tf
+++ data.tf
@@ -8,8 +8,8 @@ data "aws_caller_identity" "current" {}
 
 # Get EKS cluster info to configure Kubernetes and Helm providers
 data "aws_eks_cluster" "cluster" {
-  name = var.cluster_name
+  name = local.cluster_name
 }
 data "aws_eks_cluster_auth" "cluster" {
-  name = var.cluster_name
+  name = local.cluster_name
 }
