8024bdef770014de8a32c1315215757037fdaaf4
Ken Nguyen <kennguyen140517@gmail.com>
2022-12-02 16:08:15 +0700

    add helm charts

--- data.tf
+++ data.tf
@@ -5,11 +5,3 @@ data "aws_availability_zones" "available" {
 
 data "aws_region" "current" {}
 data "aws_caller_identity" "current" {}
-
-# # Get EKS cluster info to configure Kubernetes and Helm providers
-# data "aws_eks_cluster" "cluster" {
-#   name = local.cluster_name
-# }
-# data "aws_eks_cluster_auth" "cluster" {
-#   name = local.cluster_name
-# }
--- provider.tf
+++ provider.tf
@@ -13,6 +13,14 @@ provider "aws" {
   region = var.region
 }
 
+# Get EKS cluster info to configure Kubernetes and Helm providers
+data "aws_eks_cluster" "cluster" {
+  name = local.cluster_name
+}
+data "aws_eks_cluster_auth" "cluster" {
+  name = local.cluster_name
+}
+
 # Get EKS authentication for being able to manage k8s objects from terraform
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
